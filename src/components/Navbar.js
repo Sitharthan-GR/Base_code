8593537b38e688f9daee7fe9dab869c647f7311b
Sitharthan-GR <grsitharthan@gmail.com>
2020-11-24 17:26:46 +0530

    yes done

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>gr dapp</div>
+        <a style='float:right'>asddfdffefefsfsf</a>
+      </nav>
+    </div>
+  )
+}
